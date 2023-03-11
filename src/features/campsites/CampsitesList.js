954b506102fe5c5d22ef42506b782b1f10b23bed
Mike Boyd <mike.boyd9@gmail.com>
2023-03-11 10:40:06 -0500

    week 5

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,23 @@
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { Col, Row } from "reactstrap";
+import { selectAllCampsites } from "./campsitesSlice";
+import CampsiteCard from "./CampsiteCard";
+import { useSelector } from 'react-redux'
+
+const CampsitesList = () => {
+	const campsites = useSelector(selectAllCampsites);
+	console.log('campsites:', campsites);
+	return (
+		<Row className="ms-auto">
+			{campsites.map((campsite) => {
+				return (
+					<Col md="5" className="m-4" key={campsite.id}>
+						<CampsiteCard campsite={campsite} />
+					</Col>
+				);
+			})}
+		</Row>
+	);
+};
+
+export default CampsitesList;
